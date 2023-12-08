f213791286208b3aee02f32c3679e1a1ab76f158
tnp2004 <iceandiceisice2004@gmail.com>
2023-12-08 17:13:40 +0700

    :lipstick:Design main menu

---
+++ src/elements/Card.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let title: string;
+    export let icon: string;
+</script>
+
+<div class="card-element">
+    <label class="lcard-elem" for="header">{title}</label>
+    <div class="h-full w-full">
+        <img class="icon" src={icon} alt="icon" />
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,22 @@
-<h1>Welcome to anykit</h1>
+<script>
+    import Card from "../elements/Card.svelte";
+    import iApple from "../icons/apple.svg";
+</script>
+
+<div class="px-5 py-2">
+    <div class="mb-5">
+        <h1 class="font-bold text-2xl">anykit</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, officia?
+    </div>
+    <div class="flex flex-wrap gap-7">
+        <Card title="Apple" icon={iApple} />
+        <Card title="Apple" icon={iApple} />
+        <Card title="Apple" icon={iApple} />
+        <Card title="Apple" icon={iApple} />
+        <Card title="Apple" icon={iApple} />
+        <Card title="Apple" icon={iApple} />
+        <Card title="Apple" icon={iApple} />
+        <Card title="Apple" icon={iApple} />
+        <Card title="Apple" icon={iApple} />
+    </div>
+</div>
